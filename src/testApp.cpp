dc8092557bb4711049aba4c40df62f2f1d5f2681
Kim Ash <ash.kimberly.b@gmail.com>
2013-05-02 14:00:11 -0400

    got rid of fbo for drawing vids

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -32,29 +32,33 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
-    vidFbo.begin();
+//    vidFbo.begin();
     //want subsection of video - get rid of black bars
     for (int i = 0; i < 3; i++) {
-        player[i].draw(-player[i].getWidth()/4, 0);
+//        player[i].draw(-player[i].getWidth()/4, 0);
+        //player[i].draw((i+0.5)*player[i].getWidth()/2, 0);
+        player[i].draw(i*player[i].getWidth(), 0);
     }
-    vidFbo.end();
-    
+//    vidFbo.end();
+    /*
     ofVec3f center = ofVec3f(ofGetWidth()/4, ofGetHeight()/2, 0);
     ofRectangle fboRectangle = ofRectangle(0,0,vidFbo.getWidth(), vidFbo.getHeight());
     //mapping texture to mesh - take out of for loop to include all vids?
     mesh.addTexCoord(fboRectangle.getTopLeft());
-    mesh.addVertex(ofVec3f((0.5)*player[0].getWidth()/2, center.y, center.z) + ofVec3f(-fboRectangle.getWidth()/2, -fboRectangle.getHeight()/2, 0));
+    mesh.addVertex(center + ofVec3f(-fboRectangle.getWidth()/2, -fboRectangle.getHeight()/2, 0));
     mesh.addTexCoord(fboRectangle.getBottomLeft());
-    mesh.addVertex(ofVec3f((0.5)*player[0].getWidth()/2, center.y, center.z) + ofVec3f(-fboRectangle.getWidth()/2, fboRectangle.getHeight()/2, 0));
+    mesh.addVertex(center + ofVec3f(-fboRectangle.getWidth()/2, fboRectangle.getHeight()/2, 0));
     mesh.addTexCoord(fboRectangle.getTopRight());
-    mesh.addVertex(ofVec3f((0.5)*player[2].getWidth()/2, center.y, center.z) + ofVec3f(fboRectangle.getWidth()/2, -fboRectangle.getHeight()/2, 0));
+    mesh.addVertex(center + ofVec3f(fboRectangle.getWidth()/2, -fboRectangle.getHeight()/2, 0));
     mesh.addTexCoord(fboRectangle.getBottomRight());
-    mesh.addVertex(ofVec3f((0.5)*player[2].getWidth()/2, center.y, center.z) + ofVec3f(fboRectangle.getWidth()/2, fboRectangle.getHeight()/2, 0));
+    mesh.addVertex(center + ofVec3f(fboRectangle.getWidth()/2, fboRectangle.getHeight()/2, 0));
     mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
     
     vidFbo.getTextureReference().bind();
     mesh.draw();
     vidFbo.getTextureReference().unbind();
+     */
+                       
 //    for (int i = 0; i < 3; i++) {
 //        mesh[i].addTexCoord(fboRectangle.getTopLeft());
 //        mesh[i].addVertex(ofVec3f((i+0.5)*player[i].getWidth()/2, center.y, center.z) + ofVec3f(-fboRectangle.getWidth()/2, -fboRectangle.getHeight()/2, 0));
